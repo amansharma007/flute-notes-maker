3894cc8e3ba2e79d13ce80b983f3cd6d2ea45b6e
Aman Sharma <aman8696.56@gmail.com>
2019-05-10 00:22:35 +0530

    Added the Teevr Sargam Notes Functionality

--- script.js
+++ script.js
@@ -19,15 +19,26 @@ $("#notes-button").on("click", function(){
     notesContent.html('');
     var sur = sargam.split('');
     var space = '<div class="space"></div>'
-    var sa = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Sa</span></div>';
-    var re = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Re</span></div>';
-    var ga = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Ga</span></div>';
-    var ma = '<div class="flute text-center"><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Ma</span></div>';
-    var pa = '<div class="flute text-center"><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Pa</span></div>';
-    var dha = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Dh</span></div>';
-    var ni = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note">Ni</span></div>';
-    
+    //Normal Sargam
+    var sa = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Sa</strong></sup></span></div>';
+    var re = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Re</strong></sup></span></div>';
+    var ga = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ga</strong></sup></span></div>';
+    var ma = '<div class="flute text-center"><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ma</strong></sup></span></div>';
+    var pa = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Pa</strong></sup></span></div>';
+    var dha = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Dh</strong></sup></span></div>';
+    var ni = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ni</strong></sup></span></div>';
+    //Teevr Sargam
+    var tsa = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Sa#</strong></sup></span></div>';
+    var tre = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Re#</strong></sup></span></div>';
+    var tga = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ga#</strong></sup></span></div>';
+    var tma = '<div class="flute text-center"><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ma#</strong></sup></span></div>';
+    var tpa = '<div class="flute text-center"><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Pa#</strong></sup></span></div>';
+    var tdha = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Dh#</strong></sup></span></div>';
+    var tni = '<div class="flute text-center"><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole press">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="hole">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br><span class="note"><sup><strong>Ni#</strong></sup></span></div>';
+    //Komal Sargam -- Pending
     for(var i=0 ; i<sur.length ; ++i){
+        if(sur[i+1] !== '\''){
+
         switch(sur[i]){
             case 's' || 'S':
                 notesContent.append(sa);        
@@ -54,5 +65,33 @@ $("#notes-button").on("click", function(){
                 notesContent.append(space);        
                 break;
         }
+    } else {
+        switch(sur[i]){
+            case 's' || 'S':
+                notesContent.append(tsa);        
+                break;
+            case 'r' || 'R':
+                notesContent.append(tre);        
+                break;
+            case 'g' || 'G':
+                notesContent.append(tga);        
+                break;
+            case 'm' || 'M':
+                notesContent.append(tma);        
+                break;
+            case 'p' || 'P':
+                notesContent.append(tpa);        
+                break;
+            case 'd' || 'D':
+                notesContent.append(tdha);        
+                break;
+            case 'n' || 'N':
+                notesContent.append(tni);        
+                break;
+            case ' ':
+                notesContent.append(space);        
+                break;
+        }
+    }
     }
 })
